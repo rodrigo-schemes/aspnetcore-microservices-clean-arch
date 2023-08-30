9c69e0d9cd29b2874c6cc41736c99a861cf59ede
rodrigo-schemes <rodrigoschemes@gmail.com>
2023-08-30 20:54:06 -0300

    Adiciona Ocelot Gateway

---
+++ ApiGateways/Ocelot.ApiGateway/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Ocelot.ApiGateway;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddOcelot()
+            .AddCacheManager(o => o.WithDictionaryHandle());
+    }
+
+    public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello Ocelot");
+            });
+        });
+        await app.UseOcelot();
+    }
+}
